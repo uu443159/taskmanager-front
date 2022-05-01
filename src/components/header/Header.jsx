7460f9726050f5fc7a1756d4705d896b1ad0c774
Irina <uu443159@gmail.com>
2022-05-02 01:05:11 +0300

    Remove search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,12 +10,7 @@ export const Header = () => {
         <Navbar bg="light" expand="lg" fixed="top">
             <div>
                 <Nav style={{marginLeft: '5vw'}}>
-                    <span style={{width: '65vw'}}/>
-                    <Form inline="true" className="d-flex flex-row">
-                        <Form.Control type="text" placeholder="Search" className="mr-lg-2"/>
-                        <Button variant="success">Search</Button>
-                    </Form>
-                    <span style={{width: '5vw'}}/>
+                    <span style={{width: '70vw'}}/>
                     <AddTaskModal/>
                     <span style={{width: '5vw'}}/>
                     <div className="greetings">
